f08a487ad2fe1b4f4d1ca16a36daa5a004dd5077
lauralves <105663730+lauralves@users.noreply.github.com>
2022-12-16 21:22:43 -0300

    findAll implementation

--- src/application/ProgramDepartment.java
+++ src/application/ProgramDepartment.java
@@ -1,9 +1,24 @@
 package application;
 
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+
 public class ProgramDepartment {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		DepartmentDao dep = DaoFactory.createDepartmentDao();
+		List<Department> list = dep.findAll();
+		System.out.println("=== TEST 1: department findAll ===");
+		list = dep.findAll();
+
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,10 +1,19 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 	
@@ -16,7 +25,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
@@ -40,8 +49,30 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = conn.prepareStatement(
+					"SELECT * FROM department ORDER BY Name");
+
+			rs = ps.executeQuery();
+
+			List<Department> list = new ArrayList<>();
+
+			while (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("Id"));
+				dep.setName(rs.getString("Name"));
+				list.add(dep);
+
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(ps);
+			DB.closeResult(rs);
+		}
 	}
 
 }
